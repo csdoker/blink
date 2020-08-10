7059fd56b2da4865e67bd53f3a655b2d3dff1326
csdoker <csd758371536@qq.com>
2020-08-10 20:36:21 +0800

    first commit

---
+++ components/like/index.js
@@ -0,0 +1,38 @@
+// components/like/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    like: {
+      type: Boolean,
+      value: false
+    },
+    count: {
+      type: Number,
+      value: 0
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    likeImgSrc: 'images/like.png',
+    unlikeImgSrc: 'images/like@dis.png'
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onLikeTap(event) {
+      let { like, count } = this.properties
+      count = like ? count - 1 : count + 1
+      this.setData({
+        like: !like,
+        count
+      })
+    }
+  }
+})
